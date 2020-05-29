6a6e087e3dd263cb93b4f9f3cd6a393aa3e7b3d5
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-29 23:37:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,8 +15,8 @@ def add_post(post)
 post.author = self
 end
 
-def add_post_by_title(title)
-post = Post.new(title)
+def add_post_by_title(post_title)
+post = Post.new(post_title)
 add_post(author)
 end
 
