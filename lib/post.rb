6c63255f022e495ea059deef4983aa9e08ce3f3a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-29 23:31:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,12 @@
 class Post
 attr_accessor :title, :author
+
 @@all = [ ]
 
-  def initialize(title)
-    @title = title
-  end
+def initialize(title)
+@title = title
+@@all << self
+end
 
   def author_name
     if @author == nil
